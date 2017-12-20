3857ea81034067f413413633dd95422694cd7b93
Steven Sagun <sagunsteven@live.com>
2017-12-20 10:13:30 -0500

    Added reverse string functionality.

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,7 +1,20 @@
 package quiz;
 
 public class ReverseString {
+	
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+    	//Checks to see if the input string is null.
+    	if(input == null) {
+    		return null;
+    	}
+    	
+    	String output = "";
+    	//Takes each character from end to front and appends them to output one by one.
+    	for(int i = input.length() - 1; i >= 0; i--) {
+    		output = output + input.charAt(i);
+    	}
+    	
+    	return output;
+    	
     }
 }
